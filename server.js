4bfffca91ce7a98c2fda365c8fb079616fd39422
Or-Arbel <orarbel1995@gmail.com>
2022-10-06 14:00:03 +0300

    update server port to 17597

--- server.js
+++ server.js
@@ -53,7 +53,8 @@ module.exports.sequelize = sequelize;
 
 const app = require('./app');
 
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
+const port = process.env.PORT || 17597;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
